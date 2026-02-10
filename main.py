94d33542ef9c1bf9ae185c1567094724e588b8f2
Guillermo Hernandez <guillermohernandez@gmail.com>
2026-02-10 12:35:05 -0600

    filled out the player class, triangle display

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from constants import *
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -10,23 +11,22 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-
-    
+    first_player=Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
         log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
         screen.fill("black")
+        first_player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
         
         #print(dt)
 
     
-
-
 
 if __name__ == "__main__":
     main()
